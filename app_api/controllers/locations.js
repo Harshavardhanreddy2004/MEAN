39c20002761de64f8f0b0fad46fd42f9b5a7a9c8
sri-sathvika19 <22eg107c65@anurag.edu.in>
2024-09-19 15:02:20 +0530

    first commit

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -1,7 +1,47 @@
-const mongoose = require('mongoose');
+    const mongoose = require('mongoose');
+const Loc = mongoose.model('Location');
+const locationsListByDistance = (req, res) => {
+    res
+    .status(200)
+    .json({"status" : "success ListByDistance"});
+    };
+     const mongoose = require('mongoose');
 const Loc = mongoose.model('Location');
 const locationsCreate = (req, res) => {
     res
     .status(200)
-    .json({"status" : "success"});
+    .json({"status" : "success Create"});
     };
+     const mongoose = require('mongoose');
+const Loc = mongoose.model('Location');
+const locationsReadOne = (req, res) => {
+    res
+    .status(200)
+    .json({"status" : "success ReadOne "});
+    };
+    const mongoose = require('mongoose');
+const Loc = mongoose.model('Location');
+const  locationsUpdateOne = (req, res) => {
+    res
+    .status(200)
+    .json({"status" : "success UpdateOne"});
+    };
+    const mongoose = require('mongoose');
+const Loc = mongoose.model('Location');
+const locationsDeleteOne = (req, res) => {
+    res
+    .status(200)
+    .json({"status" : "success DeleteOne"});
+    };
+
+    
+
+    
+
+    module.exports = {
+        locationsListByDistance,
+        locationsCreate,
+        locationsReadOne,
+        locationsUpdateOne,
+        locationsDeleteOne
+        };
--- app_api/controllers/reviews.js
+++ app_api/controllers/reviews.js
@@ -1,7 +1,28 @@
 const mongoose = require('mongoose');
 const Loc = mongoose.model('Location');
-const locationsCreate = (req, res) => {
+const reviewsCreate = (req, res) => {
     res
     .status(200)
-    .json({"status" : "success"});
+    .json({"status" : "success Create"});
+    };
+    const mongoose = require('mongoose');
+const Loc = mongoose.model('Location');
+const reviewsReadOne= (req, res) => {
+    res
+    .status(200)
+    .json({"status" : "success ReadOne"});
+    };
+    const mongoose = require('mongoose');
+const Loc = mongoose.model('Location');
+const reviewsUpdateOne = (req, res) => {
+    res
+    .status(200)
+    .json({"status" : "success UpdateOne"});
+    };
+    const mongoose = require('mongoose');
+const Loc = mongoose.model('Location');
+const reviewsDeleteOne = (req, res) => {
+    res
+    .status(200)
+    .json({"status" : "success DeleteOne"});
     };
